6135584e488340fee9e2447299a8a017a6176663
Daniel Stöcklein <danielfelixstoecklein@gmail.com>
2025-01-23 16:28:01 +0000

    finally set up the reverse proxy on my server

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+async rewrites() {
+    return [
+      {
+        source: '/auth/:path*',
+        destination: 'http://87.106.207.129/auth/:path*', // Keycloak-Pfad
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- frontend/src/utils/keycloakInit.ts
+++ frontend/src/utils/keycloakInit.ts
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 export const keycloak = new Keycloak({
-    url: "http://87.106.207.129/auth",
+    url: "https://87.106.207.129/auth",
     realm: "101",
     clientId: "home"
 });
